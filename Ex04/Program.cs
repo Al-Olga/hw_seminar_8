b2bb5f5edc301c0319f17f64ac310088d9901e3d
Olga_A <zagaes2@rambler.ru>
2022-11-15 05:23:02 +0300

    correct program Ex04

--- Ex04/Program.cs
+++ Ex04/Program.cs
@@ -1,33 +1,22 @@
 ﻿// Показать треугольник Паскаля 
 // *Сделать вывод в виде равнобедренного треугольника
 
-float factorial(int n)
+double Factorial (int n)
 {
-    float i, x = 1;
-    for (i = 1; i <= n; i++)
-    {
-        x *= i;
-    }
-    return x;
+    if ((n==1) | (n==0)) return 1;
+    else return n*Factorial(n-1);
 }
 
-int i, n, c;
-Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
-string s = Console.ReadLine();
-n = int.Parse(Console.ReadLine() ?? "0");
-
-for (i = 0; i < n; i++)
+Console.Write("Введите количество строк треугольника Паскаля: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 0; i < n; i++)
 {
-    for (c = 0; c <= (n - i); c++) // создаём после каждой строки n-i отступов от левой стороны консоли, чем ниже строка, тем меньше отступ
+    for (int probel = 0; probel <= (n - i); probel++) 
+        Console.Write(" "); // отступы от левой стороны
+    for (int k = 0; k <= i; k++)
     {
         Console.Write(" "); 
-    }
-    for (c = 0; c <= i; c++)
-    {
-        Console.Write(" "); // создаём пробелы между элементами треугольника
-        Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); //формула вычисления элементов треугольника
+        Console.Write(Factorial(i) / (Factorial(k) * Factorial(i - k)));
     }
     Console.WriteLine();
-    Console.WriteLine(); // после каждой строки с числами отступаем две пустые строчки
 }
-Console.ReadLine();
